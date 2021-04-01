4e3938bac5a33e041e7970cadfd05a53b950ac8a
George Gayno <George.Gayno@noaa.gov>
2021-04-01 13:20:43 +0000

    Rename coast fix code.  Add documentation to script and source code.
    Issue #1

--- utils/scripts/run_coasts.wcoss_dell.sh
+++ utils/scripts/run_coasts.wcoss_dell.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #-----------------------------------------------------
+# Run the coastal fix code on WCOSS-Dell.
+#
 # Invoke this script as follows:
-# cat run.lsf | bsub
+# cat $script | bsub
 #-----------------------------------------------------
 
 #BSUB -oo coasts.log
 #BSUB -eo coasts.log
 #BSUB -q debug
 #BSUB -R affinity[core(1)]
-#BSUB -J rmlakes
+#BSUB -J fix_coast
 #BSUB -P GFS-DEV
 #BSUB -W 0:02
 
@@ -22,15 +24,25 @@ module list
 
 run_dir=$PWD/..
 
+#-----------------------------------------------------
+# Set your working directory.  This is where the
+# output orography file will reside.
+#-----------------------------------------------------
+
 WORKDIR=/gpfs/dell1/stmp/$LOGNAME/coasts
 rm -fr $WORKDIR
 mkdir -p $WORKDIR
 cd $WORKDIR
 
-ln -fs /gpfs/dell1/stmp/George.Gayno/fix.mask.cats/alaska_landwater_urma.gb2  ./fort.50
-ln -fs /gpfs/dell1/stmp/George.Gayno/fix.mask.cats/alaska_terrain.v2p0.gb2  ./fort.51
-#ln -fs /gpfs/dell2/emc/modeling/noscrub/Annette.Gibbs/RTMA_consensus_surface_properties/conus_landwater_v2p5_fine.gb2  ./fort.50
-#ln -fs /gpfs/dell2/emc/modeling/noscrub/Annette.Gibbs/RTMA_consensus_surface_properties/nam_smarttopoconus2p5.grb2  ./fort.51
+#-----------------------------------------------------
+# The mask file is fort.50.  The orography file is
+# fort.51.
+#-----------------------------------------------------
+
+#ln -fs /gpfs/dell1/stmp/George.Gayno/fix.mask.cats/alaska_landwater_urma.gb2  ./fort.50
+#ln -fs /gpfs/dell1/stmp/George.Gayno/fix.mask.cats/alaska_terrain.v2p0.gb2  ./fort.51
+ln -fs /gpfs/dell2/emc/modeling/noscrub/Annette.Gibbs/RTMA_consensus_surface_properties/conus_landwater_v2p5_fine.gb2  ./fort.50
+ln -fs /gpfs/dell2/emc/modeling/noscrub/Annette.Gibbs/RTMA_consensus_surface_properties/nam_smarttopoconus2p5.grb2  ./fort.51
 
 ${run_dir}/exec/fix_coasts.exe
 
--- utils/sorc/fix_coasts/fix_coasts.f90
+++ utils/sorc/fix_coasts/fix_coasts.f90
@@ -1,6 +1,10 @@
  program fix_coasts
 
-! adjust the terrain provided by raytheon using my "waterfall" logic
+!--------------------------------------------------------------
+! Adjust terrain by ensuring all water bodies have a 
+! constant elevation.  And by ensuring any adjacent land
+! points are at least 1 meter higher than the water.
+!--------------------------------------------------------------
 
  use grib_mod
 
@@ -20,13 +24,16 @@
 
  type(gribfield) :: gfld_mask, gfld_orog
 
-! read mask.  note: 0 - water; 1 - land
+!--------------------------------------------------------------
+! Read the land mask file (use the version with all water
+! bodies. 0 - water; 1 - land.
+!--------------------------------------------------------------
 
  lugb = 50
  fngrib = "./fort.50"
  call baopenr(lugb,fngrib,iret)
  if (iret /= 0) then
-   print*,'- BAD OPEN, IRET IS ', iret
+   print*,'- BAD OPEN UNIT 50, IRET IS ', iret
    stop 1
  end if
 
@@ -47,14 +54,14 @@
              unpack, k, gfld_mask, iret)
 
  if (iret /= 0) then
-   print*,'- BAD READ, IRET IS ', iret
+   print*,'- BAD READ UNIT 50, IRET IS ', iret
    stop 2
  end if
 
  call baclose(lugb, iret)
 
  if (iret /= 0) then
-   print*,'- BAD CLOSE, IRET IS ', iret
+   print*,'- BAD CLOSE UNIT 50, IRET IS ', iret
    stop 3
  end if
 
@@ -64,11 +71,15 @@
  allocate (mask(imdl,jmdl))
  mask= reshape (nint(gfld_mask%fld) , (/imdl,jmdl/) )
 
+!--------------------------------------------------------------
+! Read terrain file.
+!--------------------------------------------------------------
+
  lugb = 51
  fngrib = "./fort.51"
  call baopenr(lugb,fngrib,iret)
  if (iret /= 0) then
-   print*,'- BAD OPEN, IRET IS ', iret
+   print*,'- BAD OPEN UNIT 51, IRET IS ', iret
    stop 1
  end if
 
@@ -89,19 +100,24 @@
                unpack, k, gfld_orog, iret)
 
  if (iret /= 0) then
-   print*,'- BAD READ, IRET IS ', iret
+   print*,'- BAD READ UNIT 51, IRET IS ', iret
    stop 21
  end if
 
  call baclose(lugb, iret)
 
  if (iret /= 0) then
-   print*,'- BAD CLOSE, IRET IS ', iret
+   print*,'- BAD CLOSE UNIT 51, IRET IS ', iret
    stop 31
  end if
 
  allocate (orog(imdl,jmdl))
  orog = reshape (gfld_orog%fld, (/imdl,jmdl/) )
+
+!--------------------------------------------------------------
+! Quality control the mask and orog data to remove any
+! bad data points.
+!--------------------------------------------------------------
 
  if (imdl == 1649 .and. jmdl == 1105) then
    call qc_data_alaska(mask, orog, imdl, jmdl)
@@ -109,8 +125,14 @@
    call qc_data(mask, orog, imdl, jmdl)
  endif
 
+!--------------------------------------------------------------
+! This loop ensures water bodies have the same elevation
+! by comparing a water point to its neighbors and setting
+! its elevation to the miniumum of the two points.
+!--------------------------------------------------------------
+
  do k = 1, 5000
-   print*,'k is ',k
+   print*,'- K LOOP IS ',k
    done=.true.
    do j=2,jmdl-1
      do i=2,imdl-1
@@ -154,6 +176,11 @@
  enddo
 
  print*,'- WATERFALLS REMOVED AFTER ', k, ' PASSES.'
+
+!--------------------------------------------------------------
+! Ensure coastal land points are a least 1 meter higher
+! than the adjacent water points.
+!--------------------------------------------------------------
 
  do j = 2, jmdl-1   
    do i = 2, imdl-1 
@@ -218,6 +245,10 @@
    enddo
  enddo
 
+!--------------------------------------------------------------
+! Write update orography to a grib 2 file.
+!--------------------------------------------------------------
+
  lugb = 48
  fngrib = "./no_waterfalls.gb2"
  call baopenw(lugb,fngrib,iret)
@@ -245,18 +276,28 @@
 
  subroutine qc_data_alaska(mask, orog, imdl, jmdl)
 
+!--------------------------------------------------------------
+! Quality control the orography data for the Alaska grid.
+!
+! Note: Some logic in this routine is specific to the
+! given input orography.  So it may need to be changed
+! for another data set.
+!--------------------------------------------------------------
+
  implicit none
 
  integer, intent(in) :: imdl, jmdl
- integer(kind=1), intent(inout) :: mask(imdl,jmdl)
+ integer(kind=1), intent(in) :: mask(imdl,jmdl)
  integer :: i, j
 
  real(kind=8), intent(inout) :: orog(imdl,jmdl)
 
  print*,"- QC DATA FOR ALASKA."
 
+!--------------------------------------------------------------
 ! Remove bad ocean points and land points that were
 ! below sea level.
+!--------------------------------------------------------------
 
  do j = 1, jmdl
  do i = 1, imdl
@@ -272,8 +313,10 @@
  enddo
  enddo
 
+!--------------------------------------------------------------
 ! Remove some low (150 m) points in Great Slave lake.
 ! The wikipedia reported value is 157 m above sea level.
+!--------------------------------------------------------------
 
  do j = 750, 762
  do i = 1450, 1465
@@ -288,6 +331,14 @@
 
  subroutine qc_data(mask, orog, imdl, jmdl)
 
+!--------------------------------------------------------------
+! Quality control the orography data.
+!
+! Note: Some logic in this routine is specific to the
+! given input orography or the given grid.  So it may need 
+! to be changed for another data set.
+!--------------------------------------------------------------
+
  implicit none
 
  integer, intent(in) :: imdl, jmdl
@@ -298,12 +349,14 @@
 
  print*,"- QC MASK AND OROG."
 
+!--------------------------------------------------------------
 ! CONUS GRID ONLY!!
 ! Reset mask to remove a river between Lake Superior and the other
 ! Great Lakes. This point had a low elevation that was
 ! influencing the entire Lake Superior.  Also reset a point
 ! in Georgian Bay with a low elevation that was affecting
 ! Huron and Michigan.
+!--------------------------------------------------------------
 
  if (imdl == 2345 .and. jmdl == 1597) then ! is this the conus grid?
    print*,"- THIS IS A CONUS GRID."
@@ -311,10 +364,14 @@
    mask(1793:1798,1013:1018)=1  ! Georgian Bay
  endif
 
+!--------------------------------------------------------------
+! All grids.
+!
 ! Some ocean points had elevations below sea level which
 ! caused problems with the 'waterfall' logic.  Reset
 ! these, but be careful to retain the Salton Sea, which
 ! is minus 69 meters.
+!--------------------------------------------------------------
 
  do j = 1, jmdl
  do i = 1, imdl
